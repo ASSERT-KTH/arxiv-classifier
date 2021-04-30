75d9f55a899f2d31bd531b2a440b4bdc51037b2f
Orestis Floros <forestis@kth.se>
2021-04-30 16:57:18 +0200

    Refactor inside a main()

--- fetch.py
+++ fetch.py
@@ -9,17 +9,91 @@ import feedgenerator
 import requests
 from scipy.special import softmax
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-date = datetime.today().strftime("%Y-%m-%d")
-subprocess.run(
-    f"rm -rf data && mkdir data && cd data && oai-harvest 'http://export.arxiv.org/oai2' --from {date} -p arXiv",
-    check=True,
-    shell=True,
-)
+def main():
+    harvest_today()
+
+    entries = list(iter_load_entries_from_xml())
+    texts = [
+        single_line(entry["title"] + " abstract: " + entry["abstract"])
+        for entry in entries
+    ]
+
+    model = load_model()
+    entries = (
+        *model.predict(texts),
+        texts,
+        entries,
+    )  # prediction label, score, arxiv text, arxiv label
+
+    feed = feedgenerator.Rss201rev2Feed(
+        title="arXiv misclassified: all",
+        link="http://export.arxiv.org/rss/",
+        description="Papers from arXiv that should be classifed cs.SE according to our model.",
+        language="en",
+    )
+    sub_categories = ("cs.AI", "cs.LG", "stat.ML")
+    sub_categories_str = " ".join(sub_categories)
+    feed_sub = feedgenerator.Rss201rev2Feed(
+        title="arXiv misclassified: " + sub_categories_str,
+        link="http://export.arxiv.org/rss/",
+        description="Papers from "
+        + sub_categories_str
+        + " that should be classifed cs.SE according to our model.",
+        language="en",
+    )
+
+    for pred, score, text, entry in zip(*entries):
+        label = entry["categories"]
+        if pred and "cs.se" not in label.lower() and "cs.pl" not in label.lower():
+            abs_link = entry["link"]
+            abstract = entry["abstract"]
+            authors = entry["authors"]
+            pdf_link = abs_link.replace("/abs/", "/pdf/")
+            score = softmax(score)
+            title = entry["title"]
+
+            r = requests.get(abs_link)
+            if r.ok:
+                description = r.text
+            else:
+                description = f"""
+                {abstract}
+                <p>Authors: {authors}
+                <p><a href="{pdf_link}">{pdf_link}</a>
+                <p><a href="{abs_link}">{abs_link}</a>
+                <p>Categories: {label}
+                <p>score: {score[1]:.2f}
+                """.strip()
+
+            args = dict(
+                title=title,
+                link=pdf_link,
+                description=description,
+                unique_id=pdf_link,
+                categories=label.split(),
+            )
+            feed.add_item(**args)
+            if any(sub in label for sub in sub_categories):
+                feed_sub.add_item(**args)
+
+    os.makedirs("feed", exist_ok=True)
+    with open("feed/feed.xml", "w") as f:
+        print(feed.writeString("utf-8"), file=f)
+    with open("feed/feed2.xml", "w") as f:
+        print(feed_sub.writeString("utf-8"), file=f)
 
 
-def _entries():
+def harvest_today():
+    date = datetime.today().strftime("%Y-%m-%d")
+    subprocess.run(
+        f"rm -rf data && mkdir data && cd data && oai-harvest 'http://export.arxiv.org/oai2' --from {date} -p arXiv",
+        check=True,
+        shell=True,
+    )
+
+
+def iter_load_entries_from_xml():
     tags = ("abstract", "authors", "categories", "id", "title")
 
     for fname in glob("data/*.xml"):
@@ -31,7 +105,7 @@ def _entries():
                 if tag.endswith(wanted_tag):
                     d[wanted_tag] = el_text(el)
 
-        if all(tag in d for tag in tags):
+        if all(tag in d for tag in tags):  # Sanity check: valid entry
             d["link"] = f"https://arxiv.org/abs/{d['id']}"
             yield d
 
@@ -51,82 +125,17 @@ def single_line(s):
     return " ".join(s.split())
 
 
-entries = list(_entries())
-texts = [
-    single_line(entry["title"] + " abstract: " + entry["abstract"]) for entry in entries
-]
+def load_model():
+    from simpletransformers.classification import ClassificationModel
 
-from simpletransformers.classification import ClassificationModel
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+    return ClassificationModel(
+        "roberta",
+        "outputs/",
+        use_cuda=False,
+        args={"train_batch_size": 64, "eval_batch_size": 64, "process_count": 8},
+    )
 
-model = ClassificationModel(
-    "roberta",
-    "outputs/",
-    use_cuda=False,
-    args={"train_batch_size": 64, "eval_batch_size": 64, "process_count": 8},
-)
 
-entries = (
-    *model.predict(texts),
-    texts,
-    entries,
-)  # prediction label, score, arxiv text, arxiv label
-
-feed = feedgenerator.Rss201rev2Feed(
-    title="arXiv misclassified: all",
-    link="http://export.arxiv.org/rss/",
-    description="Papers from arXiv that should be classifed cs.SE according to our model.",
-    language="en",
-)
-sub_categories = ("cs.AI", "cs.LG", "stat.ML")
-sub_categories_str = " ".join(sub_categories)
-feed_sub = feedgenerator.Rss201rev2Feed(
-    title="arXiv misclassified: " + sub_categories_str,
-    link="http://export.arxiv.org/rss/",
-    description="Papers from "
-    + sub_categories_str
-    + " that should be classifed cs.SE according to our model.",
-    language="en",
-)
-
-# Sort by score:
-# for pred, score, text, entry in sorted(zip(*entries), key=lambda x: x[1][0]):
-for pred, score, text, entry in zip(*entries):
-    arxiv_id = entry["id"]
-    label = entry["categories"]
-    if pred and "cs.se" not in label.lower() and "cs.pl" not in label.lower():
-        abs_link = entry["link"]
-        abstract = entry["abstract"]
-        authors = entry["authors"]
-        pdf_link = abs_link.replace("/abs/", "/pdf/")
-        score = softmax(score)
-        title = entry["title"]
-
-        r = requests.get(abs_link)
-        if r.ok:
-            description = r.text
-        else:
-            description = f"""
-            {abstract}
-            <p>Authors: {authors}
-            <p><a href="{pdf_link}">{pdf_link}</a>
-            <p><a href="{abs_link}">{abs_link}</a>
-            <p>Categories: {label}
-            <p>score: {score[1]:.2f}
-            """.strip()
-
-        args = dict(
-            title=title,
-            link=pdf_link,
-            description=description,
-            unique_id=pdf_link,
-            categories=label.split(),
-        )
-        feed.add_item(**args)
-        if any(sub in label for sub in sub_categories):
-            feed_sub.add_item(**args)
-
-os.makedirs("feed", exist_ok=True)
-with open("feed/feed.xml", "w") as f:
-    print(feed.writeString("utf-8"), file=f)
-with open("feed/feed2.xml", "w") as f:
-    print(feed_sub.writeString("utf-8"), file=f)
+if __name__ == "__main__":
+    main()
